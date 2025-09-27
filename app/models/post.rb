197696ca100cb47dbf6a420e79bb74e708bf8500
Kgulwa <konkegulwa02@gmail.com>
2025-09-27 12:52:46 +0200

    Testing

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
-  belongs_to :user
+  belongs_to :u
+  
+  #ensures that the user posts have required fields
+  validates :title, presence: true
+  validates :body, presence: true
 end
---
+++ test/system/user_authentications_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class UserAuthenticationsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit user_authentications_url
+  #
+  #   assert_selector "h1", text: "UserAuthentication"
+  # end
+end
