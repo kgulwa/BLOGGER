5e3170e27f1b132f8aa9510d9e599125070eaa2f
kgulwa <161321540+kgulwa@users.noreply.github.com>
2025-09-27 16:53:54 +0200

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,24 +1,28 @@
 class PostsController < ApplicationController
-  # Require user to be signed in for creating, editing, updating, deleting
+  # Require users to be logged in for these actions
   before_action :authenticate_user!, except: [:index, :show]
 
-  # Load post from database for show, edit, update, destroy
+  # Load the post for these actions
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
-  # Ensure only the user who created the post can edit/update/destroy
+  # Ensure only the owner can edit and update or destroy
   before_action :authorize_user!, only: [:edit, :update, :destroy]
 
+  # List all posts
   def index
-    @posts = Post.all.order(created_at: :desc)
+    @posts = Post.all
   end
 
+  # Show a single post
   def show
   end
 
+  # Form for new post
   def new
     @post = current_user.posts.build
   end
 
+  # Create a post
   def create
     @post = current_user.posts.build(post_params)
     if @post.save
@@ -28,9 +32,11 @@ class PostsController < ApplicationController
     end
   end
 
+  # Form to edit post
   def edit
   end
 
+  # Updates posts
   def update
     if @post.update(post_params)
       redirect_to @post, notice: "Post was successfully updated."
@@ -39,6 +45,7 @@ class PostsController < ApplicationController
     end
   end
 
+  # Deletes posts
   def destroy
     @post.destroy
     redirect_to posts_path, notice: "Post was successfully deleted."
@@ -46,16 +53,20 @@ class PostsController < ApplicationController
 
   private
 
+  # Load post from database
   def set_post
     @post = Post.find(params[:id])
   end
 
+  # Only allow the owner to edit and update or destroy
   def authorize_user!
-    redirect_to posts_path, alert: "You are not authorized." unless @post.user == current_user
+    redirect_to posts_path, alert: "Not authorized" unless @post.user == current_user
   end
+
 
   def post_params
     params.require(:post).permit(:title, :body)
   end
 end
+
 
