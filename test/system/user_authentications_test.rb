f71379a06979e6a5201c954e85125c4dca09d3de
Kgulwa <konkegulwa02@gmail.com>
2025-09-27 13:34:41 +0200

    Added a rails import in applications.js

--- test/system/user_authentications_test.rb
+++ test/system/user_authentications_test.rb
@@ -1,9 +1,37 @@
 require "application_system_test_case"
 
 class UserAuthenticationsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit user_authentications_url
-  #
-  #   assert_selector "h1", text: "UserAuthentication"
-  # end
+  test "user can sign up" do
+    visit new_user_registration_path
+    fill_in "Email", with: "test@example.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_on "Sign up"
+
+    assert_text "WELCOME TO BLOGGER. YOU HAVE SIGNED UP SUCCESFULLY "
+  end 
+  test "user can log in" do
+    User.create!(email: "login@example.com",password: "password")
+
+    visit new_user_session_path
+    fill_in "Email", with: "login@example.com"
+    fill_in "Password", with "password"
+    click_on "Log in"
+
+
+    assert_text "SIGNED IN SUCCESSFULLY!!"
+  end
+
+  test "user can logout" do
+    user = User.create!(email: "logout@example.com", password: "password")
+
+    visit new_user_session_path
+    fill_in "Email", with: user.email
+    fill_in "Password", with: "password"
+    click_on "Log in"
+
+    click_on "Logout"
+
+    assert_text "SIGNED OUT SUCCESSFULLY"
+  end
 end
