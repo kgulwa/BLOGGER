ec9375f9a7ff72bdaf8ad845f5e793e0194cf8fc
Kgulwa <konkegulwa02@gmail.com>
2025-09-27 11:35:02 +0200

    Update posts controller with user authorization

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,4 @@
 class PostsController < ApplicationController
+before_action :authenticate_user!, except: [:index, :show] #to make sure that new,create,edit,update and destroy only work witha signed in user
+
 end
